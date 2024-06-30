76f982f3c13749d8def261fe99381c22f3b80e5c
Luxe&Kepler <168943236+78Simon87@users.noreply.github.com>
2024-06-29 23:30:29 -0500

    Add files via upload

---
+++ funcionjs/loader.js
@@ -0,0 +1,6 @@
+// Función para redirigir después de 5 segundos
+        function redirigir() {
+            setTimeout(function() {
+                window.location.href = "paginas_venta.html"; // Cambia la URL por la que desees
+            }, 3000); // 5000 milisegundos = 5 segundos
+        }
--- funcionjs/main.js
+++ funcionjs/main.js
@@ -1,1 +1,5 @@
-
+document.querySelectorAll('.toggle-btn').forEach(button => {
+  button.addEventListener('click', () => {
+    document.querySelector('.form-container').classList.toggle('rotate');
+  });
+});
