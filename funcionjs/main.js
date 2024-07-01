38baffd8ce515327e13477264819627b5fdf8582
Luxe&Kepler <168943236+78Simon87@users.noreply.github.com>
2024-06-30 22:31:50 -0500

    Add files via upload

--- funcionjs/main.js
+++ funcionjs/main.js
@@ -1,5 +1,30 @@
-document.querySelectorAll('.toggle-btn').forEach(button => {
-  button.addEventListener('click', () => {
-    document.querySelector('.form-container').classList.toggle('rotate');
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.toggle-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      document.querySelector('.form-container').classList.toggle('rotate');
+    });
+  });
+
+  const animate = document.querySelectorAll('.animer');
+
+  function triggerAnimation(entries) {
+    entries.forEach(entry => {
+      const anim_divs = entry.target.querySelectorAll('.div_ventajas');
+      anim_divs.forEach(div => {
+        div.classList.toggle('unset', entry.isIntersecting);
+      });
+    });
+  }
+
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1
+  }
+
+  const observer = new IntersectionObserver(triggerAnimation, options);
+
+  animate.forEach(anim_div => {
+    observer.observe(anim_div);
   });
 });
